0caeabd1ec5f689b1a847fa861e72f9ea4a7215c
Hanisha20 <hanishabi96@gmail.com>
2024-03-04 00:53:01 +0330

    home page created

--- shop/home/views.py
+++ shop/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+# from django.views.decorators.csrf import csrf_exempt
+# from django.http import JsonResponse
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from accounts.models import Person
+# import json
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/homePage.html')
